6f2b6e5ed7c7324b804e97ed4da34300cbd2ca62
Davi <davisiqueira591@gmail.com>
2023-03-11 14:30:24 -0300

    butei as bombas

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import "./App.css";
 
 function App() {
+  const [lose, setLose] = useState(false)
+
+  const handleClickBomb = () => {
+    setLose(true)
+  }
 
   useEffect(() => {
     const mine = document.querySelector(".mine")
@@ -10,18 +15,42 @@ function App() {
       while(mine.childElementCount < 200) {
         let block = document.createElement("button")
         block.setAttribute("class", "block")
-
+        block.setAttribute("id", mine.childElementCount.toString())
 
         mine.appendChild(block)
       }
+      
+      let bombs = 20
+      while(bombs > 0) {
+        let blockBomb = Math.floor(Math.random() * 200)
+        if(mine.children[blockBomb].innerHTML != "<p>💣</p>") {
+          mine.children[blockBomb].innerHTML = "<p>💣</p>"
+          mine.children[blockBomb].addEventListener("click", handleClickBomb)
+          bombs--;
+        }
+      }
     }
   }, [])
+
+  useEffect(() => {
+    if(lose === true) {
+      const mine = document.querySelector(".mine")
+
+      const loseTitle = document.createElement("h1")
+      loseTitle.innerHTML = "YOU LOSE"
+  
+      mine?.appendChild(loseTitle)
+    }
+  }, [lose])
 
   return (
     <div className="App" role="main">
       <h1>MineSweeper</h1>
       <div className="mine">
       </div>
+      <footer>
+        <p>Made by <a target="_blank" href="https://github.com/davitostes">Davi Tostes</a></p>
+      </footer>
     </div>
   );
 }
